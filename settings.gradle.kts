873b212078959439a9fa219654c9c09c74c32740
AMixailV <Mixer-X@mail.ru>
2024-10-14 20:19:30 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "compose_16_LazyColumn_part3"
+include(":app")
+ 
